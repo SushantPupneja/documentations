d3b23b0e781522ba5e08124db07525810954c1a4
uzumaki <sushant_pupneja@yahoo.com>
2021-04-20 01:07:28 +0530

    adding iot docs

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ let app = require('express')();
 
 const swaggerUi = require('swagger-ui-express');
 const drsSwaggerDocument = require('./docs/device_registration/swagger.json');
-
+const iotSwaggerDocument = require('./docs/iot_access_layer/swagger.json');
 // registrations
 app.use('/drs/api-docs', swaggerUi.serve, swaggerUi.setup(drsSwaggerDocument));
 
+// app.use('/iot/api-docs', swaggerUi.serve, swaggerUi.setup(iotSwaggerDocument));
+
 var port = 8000;
+// var port = 9000;
 
 app.listen(port, () => {
 	console.log(`listening at ${port}`);
